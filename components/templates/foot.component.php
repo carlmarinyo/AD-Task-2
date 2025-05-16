89c1319322ecd2df623f04fa58ba6893176674c5
carlmarinyo <marinojerome2@gmail.com>
2025-05-16 23:04:09 +0800

    feat (foot component): added footer with links for other pages

--- components/templates/foot.component.php
+++ components/templates/foot.component.php
@@ -3,7 +3,11 @@ function footer()
 {
     ?>
     </body>
-
+    <footer class="text-center p-4 bg-gray-100 text-sm text-gray-600 mt-8">
+    © 2025 Carl's Garage. All rights reserved.
+    <a href="/index.php">Home</a>
+    <a href="/pages/pageSampleHere/index.php">Garage</a>
+    </footer>
     </html>
     <?php
 }
