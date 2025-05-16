ecde5c3e9bb99648564543b08035f72db5610369
carlmarinyo <marinojerome2@gmail.com>
2025-05-16 23:53:16 +0800

    refactor (landing-page): removed require of non-existing handler

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 require BASE_PATH . '/vendor/autoload.php';
 require BASE_PATH . '/utils/htmlEscape.utils.php';
 
-require_once BASE_PATH . '/handlers/pageData.handler.php';
 require_once BASE_PATH . '/components/cards/note.component.php';
 require_once BASE_PATH . '/components/templates/foot.component.php';
 require_once BASE_PATH . '/components/templates/head.component.php';
@@ -11,7 +10,7 @@ require_once BASE_PATH . '/components/templates/nav.component.php';
 
 <?php
 head('Product List');
-navHeader($navList);
+navHeader();
 ?>
 
 
