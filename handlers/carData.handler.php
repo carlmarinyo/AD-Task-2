adfee5fdc74d95e155bd6b8fc3eb646ba6344537
carlmarinyo <marinojerome2@gmail.com>
2025-05-16 22:50:03 +0800

    feat (handler): create data handler for car brand and car info

---
+++ handlers/carData.handler.php
@@ -0,0 +1,27 @@
+<?php
+$carBrands = ["Toyota", "Ford", "BMW", "Honda"];
+
+$carInfo = [
+
+"Toyota" => [
+    "desc" => "Japanese automotive manufacturer known for reliable, fuel-efficient cars and pioneering hybrid technology.",
+    "img" => ""
+    ],
+
+"Ford" => [
+    "desc" => " American automaker famous for durable trucks, muscle cars, and pioneering mass production in the auto industry.",
+    "img" => ""   
+    ],
+"BMW" => [
+    "desc" => "German luxury automaker known for sporty performance, advanced engineering, and premium vehicles.",
+    "img" => ""
+],
+"Honda" => [
+    "desc" => "Honda is a Japanese automaker renowned for reliable, efficient cars, motorcycles, and innovative engineering.",
+    "img" => ""
+]
+
+
+]
+
+?>
