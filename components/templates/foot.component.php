e4ea27b314ab731e3cecc3905a0087098ed39073
carlmarinyo <marinojerome2@gmail.com>
2025-05-16 23:58:01 +0800

    refactor(foot component) - corrected the links connected

--- components/templates/foot.component.php
+++ components/templates/foot.component.php
@@ -5,7 +5,7 @@ function footer()
     <footer>
     © 2025 Carl's Garage. All rights reserved.
     <a href="/index.php">Home</a>
-    <a href="/pages/pageSampleHere/index.php">Garage</a>
+    <a href="/pages/gallery/index.php">Gallery</a>
     </footer>
     </html>
     <?php
