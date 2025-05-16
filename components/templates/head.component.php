9df1bdcf441cd2b5698b48cb18d1c9065d26e356
carlmarinyo <marinojerome2@gmail.com>
2025-05-16 23:27:08 +0800

    feat (footer component): added footer with link to other page

--- components/templates/head.component.php
+++ components/templates/head.component.php
@@ -9,10 +9,8 @@ function head($pageTitle)
             <meta charset="utf-8">
             <title><?= htmlEscape($pageTitle ?? 'My Shop') ?></title>
             <link  rel ="stylesheet" href="assets/css/styles.css">
-
         </head>
-
-        <body>
+            
             <?php
 }
 ?>
