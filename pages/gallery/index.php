ba9c34c56fe3ac1daddf47130c5277b8cc6c26a0
carlmarinyo <marinojerome2@gmail.com>
2025-05-18 11:01:55 +0800

    feat (gallery-page): add basic gallery page structure with head,nav, footer components integrated

---
+++ pages/gallery/index.php
@@ -0,0 +1,19 @@
+<?php
+require BASE_PATH . '/vendor/autoload.php';
+require BASE_PATH . '/utils/htmlEscape.utils.php';
+
+require_once BASE_PATH . '/components/cards/note.component.php';
+require_once BASE_PATH . '/components/templates/foot.component.php';
+require_once BASE_PATH . '/components/templates/head.component.php';
+require_once BASE_PATH . '/components/templates/nav.component.php';
+?>
+
+<?php
+head('Gallery');
+navHeader();
+?>
+
+
+<?php
+footer();
+?>
