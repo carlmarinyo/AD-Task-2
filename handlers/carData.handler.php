bb59a7964747795e6a064563139b8804cac44af6
carlmarinyo <marinojerome2@gmail.com>
2025-05-19 19:51:54 +0800

    refactor (car data handler): refactor car data to include image paths for gallery cards

--- handlers/carData.handler.php
+++ handlers/carData.handler.php
@@ -5,20 +5,20 @@ $carInfo = [
 
 "Toyota" => [
     "desc" => "Japanese automotive manufacturer known for reliable, fuel-efficient cars and pioneering hybrid technology.",
-    "img" => ""
+    "img" => "/pages/gallery/assets/img/toyota.png"
     ],
 
 "Ford" => [
     "desc" => " American automaker famous for durable trucks, muscle cars, and pioneering mass production in the auto industry.",
-    "img" => ""   
+    "img" => "/pages/gallery/assets/img/ford.jpg"   
     ],
 "BMW" => [
     "desc" => "German luxury automaker known for sporty performance, advanced engineering, and premium vehicles.",
-    "img" => ""
+    "img" => "/pages/gallery/assets/img/bmw.png"
 ],
 "Honda" => [
     "desc" => "Honda is a Japanese automaker renowned for reliable, efficient cars, motorcycles, and innovative engineering.",
-    "img" => ""
+    "img" => "/pages/gallery/assets/img/honda.png"
 ]
 
 
