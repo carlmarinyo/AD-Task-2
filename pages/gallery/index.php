0f99e3fee99456e8f156858a5d4c60b3b57f1c1d
carlmarinyo <marinojerome2@gmail.com>
2025-05-19 19:48:17 +0800

    feat (gallery-page): add gallery card component and display logic to gallery page

--- pages/gallery/index.php
+++ pages/gallery/index.php
@@ -2,17 +2,23 @@
 require BASE_PATH . '/vendor/autoload.php';
 require BASE_PATH . '/utils/htmlEscape.utils.php';
 
-require_once BASE_PATH . '/components/cards/note.component.php';
+require_once BASE_PATH . '/components/cards/gallery-card.component.php';
 require_once BASE_PATH . '/components/templates/foot.component.php';
 require_once BASE_PATH . '/components/templates/head.component.php';
 require_once BASE_PATH . '/components/templates/nav.component.php';
+require_once BASE_PATH . '/handlers/carData.handler.php';
 ?>
 
 <?php
 head('Gallery');
 navHeader();
 ?>
+<body>
+    <div class="card-container">
+        <?php displayCardForm($carBrands, $carInfo);?>
+    </div>
 
+</body>
 
 <?php
 footer();
