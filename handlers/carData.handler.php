d5e3a56dce82b1c334e1f524d59e069ed90c5479
carlmarinyo <marinojerome2@gmail.com>
2025-05-19 20:33:29 +0800

    refactor (car data handler): update description of each cars

--- handlers/carData.handler.php
+++ handlers/carData.handler.php
@@ -4,16 +4,16 @@ $carBrands = ["Toyota", "Ford", "BMW", "Honda"];
 $carInfo = [
 
 "Toyota" => [
-    "desc" => "Japanese automotive manufacturer known for reliable, fuel-efficient cars and pioneering hybrid technology.",
+    "desc" => "Toyota is a Japanese automotive manufacturer known for reliable, fuel-efficient cars and pioneering hybrid technology.",
     "img" => "/pages/gallery/assets/img/toyota.png"
     ],
 
 "Ford" => [
-    "desc" => " American automaker famous for durable trucks, muscle cars, and pioneering mass production in the auto industry.",
+    "desc" => "Ford is an American automaker famous for durable trucks, muscle cars, and pioneering mass production in the auto industry.",
     "img" => "/pages/gallery/assets/img/ford.jpg"   
     ],
 "BMW" => [
-    "desc" => "German luxury automaker known for sporty performance, advanced engineering, and premium vehicles.",
+    "desc" => "BMW is a German luxury automaker known for sporty performance, advanced engineering, and premium vehicles.",
     "img" => "/pages/gallery/assets/img/bmw.png"
 ],
 "Honda" => [
